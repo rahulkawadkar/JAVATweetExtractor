ef06fd0e6a2e4291da34f06dbe62baee8ab448b8
rahulkawadkar <rahul.2605@yahoo.co.in>
2017-05-12 02:29:27 -0400

    Removed unused imports
    Removed unused system imports and outputs.

--- TwitterStream.java
+++ TwitterStream.java
@@ -1,14 +1,5 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import javax.security.auth.login.Configuration;
-
-import twitter4j.Paging;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -24,50 +15,42 @@ public class TwitterStream {
 		cb.setOAuthConsumerKey("CONSUMER KEY");
 		cb.setOAuthConsumerSecret("CONSUMER SECRET");
 		cb.setOAuthAccessToken("OAUTH ACCESS TOKEN");
-		cb.setOAuthAccessTokenSecret("TOKEN SECRET");
-		cb.setHttpProxyHost("PROXY HOST");
+		cb.setOAuthAccessTokenSecret("OAUTH ACCESS TOKEN SECRET");
+		cb.setHttpProxyHost("proxy.company.host");
 		cb.setHttpProxyPort(1234);
-		cb.setHttpProxyUser("useruser");
-		cb.setHttpProxyPassword("Us3rPa$$w0rD");
+		cb.setHttpProxyUser("proxyuser");
+		cb.setHttpProxyPassword("ProxyUs3rPa$$w0rD");
 
 		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
-		System.out.println("TwitterFactory initialized!!!");
-
-		/*int pageno = 1;
-		String user = "cnn";
-		List<Object> statuses = new ArrayList<Object>();*/
 
 		while (true) {
 
 			try {
-				Query query = new Query("Morning");
+				Query query = new Query("IPL2017");
 				QueryResult result;
-				//FileWriter fw = new FileWriter(file, true);
-				//BufferedWriter bw = new BufferedWriter(fw);
+				int totalTweets = 0;
+
 				do {
 					result = twitter.search(query);
 					List<Status> tweets = result.getTweets();
+					System.out.println("Tweets extracted ::" + tweets.size());
+					System.out.println("TWEETS BELOW ::");
 					for (Status tweet : tweets) {
 						System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
 						System.out.println("Tweet Favorite Count :: " + tweet.getFavoriteCount());
 						System.out.println("Retweet Count :: " + tweet.getRetweetCount() + "\n");
+
 					}
+					totalTweets = tweets.size() + totalTweets;
+					System.out.println("FINAL COUNT::: " + totalTweets);
 				} while ((query = result.nextQuery()) != null);
 				System.exit(0);
-				//bw.close();
 
-				/*
-				 * int size = statuses.size(); Paging page = new
-				 * Paging(pageno++, 100);
-				 * statuses.addAll(twitter.getUserTimeline(user, page)); if
-				 * (statuses.size() == size) break;
-				 */
 			} catch (TwitterException e) {
 
 				e.printStackTrace();
 			}
 		}
 
-		// System.out.println("Total: " + statuses.size());
 	}
 }
